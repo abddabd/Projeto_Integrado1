5e73fa70b5a863b20797daee9eae06cdcb1123aa
Guilherme <143672412+abddabd@users.noreply.github.com>
2024-04-29 11:42:18 -0300

    Piscar Cursor 29/04

--- Placa_Display/Core/Src/main.c
+++ Placa_Display/Core/Src/main.c
@@ -53,6 +53,7 @@ SPI_HandleTypeDef hspi1;
 short cursorX = 1;
 short cursorY = 1;
 const char* jogador = "x";
+int delay = 1;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -100,38 +101,35 @@ int main(void)
   ST7735_Init();
   short Matriz[3][3];
   // desenhaQuad(3, 3, "x");
-  int delay = 0;
   ST7735_FillScreen(WHITE);
   ST7735_DrawLine(50, 29, 110, 29, BLACK);
   ST7735_DrawLine(50, 49, 110, 49, BLACK);
   ST7735_DrawLine(70, 9, 70, 69, BLACK);
   ST7735_DrawLine(90, 9, 90, 69, BLACK);
-  desenhaQuad(cursorX, cursorY, jogador);
+  //desenhaQuad(cursorX, cursorY, jogador);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+      HAL_Delay(200);
+
 	  if (botEsquerda) {
-	 desenhaQuad(cursorX, cursorY, " ");
 	 cursorParaEsquerda();
-	 desenhaQuad(cursorX, cursorY, jogador);
 	  }
+
 	  if (botDireita) {
-	 desenhaQuad(cursorX, cursorY, " ");
-	 cursorParaDireita();
-	 desenhaQuad(cursorX, cursorY, jogador);
-	    	 // desenhaQuad(3, 3, "x");
+	  cursorParaDireita();
 	  }
+
 	  if (botBaixo) {
-	    if (jogador == "x") {
-	    	jogador = "o";
-	    } else {
-	    	jogador = "x";
-	    }
-	  desenhaQuad(cursorX, cursorY, jogador);
+	  alternaJogador();
+	  HAL_Delay(200);
 	  }
+
+	  piscaCursor();
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -154,7 +152,9 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -163,12 +163,12 @@ void SystemClock_Config(void)
   */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -248,16 +248,10 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA9 PA12 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_12;
+  /*Configure GPIO pins : PA9 PA10 PA11 PA12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : PA10 PA11 */
-  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
@@ -291,6 +285,36 @@ void cursorParaEsquerda () {
 	} else if (cursorX == 0) {
 		cursorX = 3;
 		cursorY--;
+	}
+}
+void alternaJogador() {
+	if (jogador == "x") {
+	   	jogador = "o";
+	    } else {
+	   	jogador = "x";
+	    }
+}
+void piscaCursor() {
+	while (1) {
+		desenhaQuad (cursorX, cursorY, jogador);
+		for (int x=0;x<20;x++)
+		{
+			HAL_Delay(50);
+			if (botCima || botDireita || botEsquerda || botBaixo)
+			{
+				desenhaQuad (cursorX, cursorY, " ");
+				return;
+			}
+		}
+		desenhaQuad (cursorX, cursorY, " ");
+		for (int x=0;x<20;x++)
+		{
+			HAL_Delay(50);
+			if (botCima || botDireita || botEsquerda || botBaixo)
+			{
+				return;
+			}
+		}
 	}
 }
 /* USER CODE END 4 */
